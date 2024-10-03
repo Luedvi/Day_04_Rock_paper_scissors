e8c6d30acf311bfd3f0bbc1e8c9ed59f2c6ae7d7
Luedvi <luedvi@outlook.com>
2024-10-03 05:13:50 -0600

    Update main.py version 1.2

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import random
 
 options = ("rock", "paper", "scissors")
-user = input("choose rock, paper or scissors: ").lower()
+user_wins = 0
+computer_wins = 0
 
-if user not in options:
-    print("sorry that's not a valid option")
-else:
-    computer = random.choice(options)
-    print("computer:", computer)
+while user_wins < 2 and computer_wins < 2:
+    user = input("choose rock, paper or scissors: ").lower()
 
-    if user == computer:
-        print ("draw")
-
-    elif user == "rock" and computer == "scissors":
-            print("you win")
-
-    elif user == "paper" and computer == "rock":
-            print("you win")
-
-    elif user == "scissors" and computer == "paper":
-            print("you win")
-
+    if user not in options:
+        print("sorry that's not a valid option")
     else:
-        print("you lose")
+        computer = random.choice(options)
+        print("computer:", computer)
+
+        if user == computer:
+            print("draw")
+
+        elif user == "rock" and computer == "scissors" or user == "paper" and computer == "rock" or user == "scissors" and computer == "paper":
+            print("you win")
+            user_wins += 1
+
+        else:
+            print("you lose")
+            computer_wins += 1
+
+if user_wins > computer_wins:
+    print("User wins", user_wins, "out of 3 matches")
+else:
+    print("User loses", computer_wins, "out of 3 matches")
